bfc7aff6227f15e17aed56be751010b414246691
maxtiish <cearness@mail.ru>
2024-02-26 00:37:45 +0700

    add first files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.gradle.api.tasks.testing.logging.TestLogEvent
-
 plugins {
     id("java")
     id("checkstyle")
+    id("application")
+    id("io.freefair.lombok") version "8.3"
+    jacoco
 }
 
 group = "hexlet.code"
@@ -13,28 +13,35 @@ repositories {
     mavenCentral()
 }
 
+application {
+    mainClass.set("hexlet.code.App")
+}
+
 dependencies {
-    implementation("com.h2database:h2:2.2.222")
-    implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-    implementation("org.apache.commons:commons-text:1.10.0")
+    implementation("org.postgresql:postgresql:42.5.4")
     implementation("gg.jte:jte:3.1.0")
-    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation("com.h2database:h2:2.2.222")
     implementation("io.javalin:javalin:5.6.2")
     implementation("io.javalin:javalin-bundle:5.6.2")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("io.javalin:javalin-rendering:5.6.2")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("com.konghq:unirest-java-core:4.2.4")
+    implementation("com.konghq:unirest-java-bom:4.2.4")
+    implementation("org.jsoup:jsoup:1.17.2")
 
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
 }
 
 tasks.test {
-    tasks.test {
-        useJUnitPlatform()
-        testLogging {
-            exceptionFormat = TestExceptionFormat.FULL
-            events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
-            showStandardStreams = true
-        }
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
     }
 }
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,28 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    public static void main(String[] args) throws SQLException, IOException {
+        var app = getApp();
+        app.start(getPort());
+    }
+    public static Javalin getApp() throws IOException, SQLException {
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> {
+            ctx.result("Hello, World!");
+        });
+        return app;
+    }
+}
